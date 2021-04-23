9b5fcfd7c991b42e5083fafba4ad386a546b90f9
Lucas <lucassntg28@gmail.com>
2021-04-23 14:26:33 -0300

    create component Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import {
 
-export default function Login(){
-    return(
-        <div>
-            <h1>Login</h1>
+    Container,
+    Col,
+    Row,
+    Card,
+    CardBody,
+    Form,
+    Input,
+    InputGroup,
+    Button
+
+} from 'reactstrap'
+export default function Login() {
+    return (
+        <div className='mt-5 flex-row align-items-center'>
+            <Container>
+                <Row className='justify-content-center'>
+                    <Col md='6'>
+                        <Card>
+                            <CardBody>
+                                <Form>
+                                    <h1>Login</h1>
+                                    <p>Enter e-mail and password to access</p>
+                                    <InputGroup className='mb-3'>
+                                        <Input type='text' placeholder='E-mail' />
+                                    </InputGroup>
+                                    <InputGroup className='mb-3'>
+                                        <Input type='password' placeholder='Password' />
+                                    </InputGroup>
+                                    <InputGroup className='mb-3'>
+                                    <Button color="primary"> GO!</Button>
+                                    </InputGroup>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
